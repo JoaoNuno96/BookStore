26a8cd887c4bc58cb5f9fe7636d4f67171370fd2
JoaoNuno96 <joaonuno1996@hotmail.com>
2025-05-01 12:00:48 +0100

    PublisherController DI changed to HttpClient to connect to my API endpoint

--- BookStore/Controllers/PublisherController.cs
+++ BookStore/Controllers/PublisherController.cs
@@ -4,23 +4,33 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Text;
+using Microsoft.AspNetCore.Http;
 
 namespace BookStore.Controllers
 {
     public class PublisherController : Controller
     {
-        private readonly PublisherService _pubservice;
+        private readonly HttpClient _httpclientPub;
 
-        public PublisherController(PublisherService ps)
+        public PublisherController(IHttpClientFactory httpclientPublisher)
         {
-            this._pubservice = ps;
+            this._httpclientPub = httpclientPublisher.CreateClient("PublisherApi");
         }
 
         public async Task<IActionResult> Index()
         {
-            List<Publisher> listpub = await this._pubservice.GetPublishersAsync();
+            List<Publisher> listPub = null;
 
-            return View(listpub);
+            using (HttpResponseMessage httpresponse = await this._httpclientPub.GetAsync("getAll"))
+            {
+                string json = await httpresponse.Content.ReadAsStringAsync();
+                listPub = JsonConvert.DeserializeObject<List<Publisher>>(json);
+            }
+
+            return View(listPub);
         }
     }
 }
